889792a814c0817db6cec0c0e1dce1678ab0180b
CrisGadea <cristianhernangadea@gmail.com>
2023-12-26 13:49:04 -0300

    finish crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.service.getUsers();
+  }
+
+  @Get(':id') // ParseIntPipe convierte a number el string recibido
+  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.service.getUser(id);
+  }
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
+    return this.service.createUser(newUser);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ) {
+    return this.service.updateUser(id, user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.service.deleteUser(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  createUser(user: CreateUserDto) {
+    const newUser = this.repository.create(user);
+    return this.repository.save(newUser);
+  }
+
+  getUsers() {
+    return this.repository.find();
+  }
+
+  getUser(id: number) {
+    return this.repository.findOne({
+      where: {
+        // id: id, == id
+        id,
+      },
+    });
+  }
+
+  updateUser(id: number, user: UpdateUserDto) {
+    return this.repository.update({ id }, user);
+  }
+
+  deleteUser(id: number) {
+    return this.repository.delete({ id });
+  }
+}
